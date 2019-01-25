164ce13e5a3d23c4617b473cf8e7e2aaf3b207ad
schwarzblade <schwarzblade@gmail.com>
2019-01-25 18:33:33 +0100

    feature/login

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,11 @@
 /* Here goes your JS code */
+//alert("sdfds");
+// Get the modal
+var modal = document.getElementById('show-popup-form');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
